c8bc39b599564e607594d3a2e966c756eb1c1372
Mattia <Mattia@DESKTOP-F1MFGAO.home>
2020-05-31 23:51:59 +0200

    Soluzione

--- Lab07_Poweroutages/src/main/java/it/polito/tdp/poweroutages/FXMLController.java
+++ Lab07_Poweroutages/src/main/java/it/polito/tdp/poweroutages/FXMLController.java
@@ -1,23 +1,49 @@
-/**
- * Sample Skeleton for 'Scene.fxml' Controller Class
- */
-
 package it.polito.tdp.poweroutages;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class FXMLController {
 
-    @FXML // ResourceBundle that was given to the FXMLLoader
+    @FXML
     private ResourceBundle resources;
 
-    @FXML // URL location of the FXML file that was given to the FXMLLoader
+    @FXML
     private URL location;
 
-    @FXML // This method is called by the FXMLLoader when initialization is complete
+    @FXML
+    private ComboBox<?> cbxScelta;
+
+    @FXML
+    private TextField txtYears;
+
+    @FXML
+    private TextField txtHours;
+
+    @FXML
+    private Button btnWorstCase;
+
+    @FXML
+    private TextArea txtRisultato;
+
+    @FXML
+    void doWorstCase(ActionEvent event) {
+
+    }
+
+    @FXML
     void initialize() {
+        assert cbxScelta != null : "fx:id=\"cbxScelta\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtYears != null : "fx:id=\"txtYears\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtHours != null : "fx:id=\"txtHours\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert btnWorstCase != null : "fx:id=\"btnWorstCase\" was not injected: check your FXML file 'Scene.fxml'.";
+        assert txtRisultato != null : "fx:id=\"txtRisultato\" was not injected: check your FXML file 'Scene.fxml'.";
 
     }
 }
